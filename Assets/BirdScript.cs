ca5e2a8c7111a53e21db62d513622f43a43ca895
CarlosLaraFP <info@carloslaraai.com>
2023-10-20 09:48:05 -0700

    Implemented physics and user input

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class BirdScript : MonoBehaviour
 {
+    public Rigidbody2D rigidBody;
+    public float levitationFactor;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        this.name = "Spaceship";
+        levitationFactor = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigidBody.velocity = Vector2.up * levitationFactor;
+        }
     }
 }
