0bdc85b7d0a56cbbeda7490521248df5eb9d48dd
CarlosLaraFP <info@carloslaraai.com>
2023-10-21 08:56:09 -0700

    Randomized spawner Y coordinate

--- Assets/PipeSpawnerScript.cs
+++ Assets/PipeSpawnerScript.cs
@@ -6,17 +6,23 @@ public class PipeSpawnerScript : MonoBehaviour
 {
     // Prefabricated game object
     public GameObject pipe;
+
+    // Both in Newtonian seconds
     public float spawnRate = 2.0f;
     private float timer = 0;
 
+    // Y coordinate bounds
+    public float minY = -10.0f;
+    public float maxY = 10.0f;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
         if (timer < spawnRate)
         {
@@ -25,11 +31,21 @@ public class PipeSpawnerScript : MonoBehaviour
         }
         else
         {
-            // Instantiating prefabs at runtime
-            Instantiate(pipe, this.transform.position, this.transform.rotation);
-            // we pass the position and rotation of the object holding the script (the Pipe Spawner)
+            spawnPipes();
 
             timer = 0;
         }
+    }
+
+    private void spawnPipes()
+    {
+        // Instantiating prefabs at runtime
+        Instantiate(pipe, getRandomYPosition(this.transform.position), this.transform.rotation);
+        // we pass the position and rotation of the object holding the script (the Pipe Spawner)
+    }
+
+    private Vector3 getRandomYPosition(Vector3 position)
+    {
+        return new Vector3(position.x, position.y + Random.Range(minY, maxY), position.z);
     }
 }
